1a91787427857c5e0455248207e4cedd64a2732c
kwonshzzang <kwonshzzang@gmail.com>
2025-06-26 11:48:59 +0900

    Add Chapter03

---
+++ chapter03/scripts/create-topics.sh
@@ -0,0 +1,22 @@
+echo "Waiting for Kafka to come online..."
+
+cub kafka-ready -b kafka:9092 1 20
+
+# create the tweet topic
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic tweets \
+  --replication-factor 1 \
+  --partitions 4 \
+  --create
+
+
+# create the crypto-sentiment topic
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic crypto-sentiment \
+  --replication-factor 1 \
+  --partitions 4 \
+  --create
+
+sleeep infinity
